3ae7d4901981323c1a07a23b28a73dc7e5a7d881
Sergey Kislov <serejka1991@gmail.com>
2022-11-10 18:25:27 +0300

    lesson_1

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const nameGoods = "MacBook Air M2";
+const countGoods = 3; // шт
+const categoryGoods = "Laptop";
+const categoryPrice = 112_000; // ₽
+
+console.log(nameGoods);
+console.log(countGoods*categoryPrice);
