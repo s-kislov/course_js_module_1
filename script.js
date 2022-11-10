c508009bca59ada759dc0b408428c131a8e02903
Sergey Kislov <serejka1991@gmail.com>
2022-11-10 18:38:46 +0300

    lesson_2 extra

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
-const nameGoods = "MacBook Air M2";
-const countGoods = 3; // шт
-const categoryGoods = "Laptop";
-const categoryPrice = 112_000; // ₽
+{
+    const nameGoods = "MacBook Air M2";
+    const countGoods = 3; // шт
+    const categoryGoods = "Laptop";
+    const categoryPrice = 112_000; // ₽
 
-console.log(nameGoods);
-console.log(countGoods*categoryPrice);
+    console.log(nameGoods);
+    console.log("Общая сумма товаров: " + (countGoods * categoryPrice));
+}
+
+{
+    const nameGoods = "BMW S1000RR";
+    const countGoods = 1; // шт
+    const categoryGoods = "Moto";
+    const categoryPrice = 980_000; // ₽
+
+    console.log(nameGoods);
+    console.log("Общая сумма товаров: " + (countGoods * categoryPrice));
+}
